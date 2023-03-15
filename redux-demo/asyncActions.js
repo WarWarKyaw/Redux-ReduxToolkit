39104a4120a2b310867455a295abb6e5e393aa69
War War Kyaw <warwar67890@gmail.com>
2023-03-15 22:39:24 +0630

    middleware using redux-thunk for asynchronous actions

---
+++ redux-demo/asyncActions.js
@@ -0,0 +1,89 @@
+const redux = require("redux");
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+// action type
+const FETCH_USERS_REQUESTED = "FETCH_USERS_REQUESTED";
+const FETCH_USERS_SUCCEEDED = "FETCH_USERS_SUCCEEDED";
+const FETCH_USERS_FAILED = "FETCH_USERS_FAILED";
+
+// action creator
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUESTED,
+  };
+};
+
+// action creator
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCEEDED,
+    payload: users,
+  };
+};
+
+// action creator
+const fetchUsersFail = (error) => {
+  return {
+    type: FETCH_USERS_FAILED,
+    payload: error,
+  };
+};
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUESTED:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_USERS_SUCCEEDED:
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+    case FETCH_USERS_FAILED:
+      return {
+        ...state,
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// async action creator (thunk), it will return function, that include side-effect
+const fetchUsers = () => {
+  return async function (dispatch) {
+    dispatch(fetchUsersRequest());
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.co");
+      // response.data is the users
+      const users = response.data.map((item) => item.name);
+      //   console.log("response ", response);
+      dispatch(fetchUsersSuccess(users));
+    } catch (error) {
+      // error.message is the error message
+      dispatch(fetchUsersFail(error.message));
+    }
+  };
+};
+
+const store = redux.createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(fetchUsers());
