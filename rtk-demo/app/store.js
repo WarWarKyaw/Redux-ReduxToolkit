df3dc63f5991e0137874c6732e2cb85e70e1cc5d
War War Kyaw <warwar67890@gmail.com>
2023-03-16 22:06:00 +0630

    using middleware redux-logger

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,9 +1,17 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("./features/cake/cakeSlice");
+const { getDefaultMiddleware } = require("@reduxjs/toolkit");
+const reduxlogger = require("redux-logger");
 
 const iceCreamReducer = require("./features/icecream/icecreamSlice");
+// const store = configureStore({
+//   reducer: { cake: cakeReducer, iceCream: iceCreamReducer },
+// });
+const logger = reduxlogger.createLogger();
+
 const store = configureStore({
   reducer: { cake: cakeReducer, iceCream: iceCreamReducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -5,9 +5,9 @@ const iceCreamActions =
 
 console.log("Initial State ", store.getState());
 
-store.subscribe(() => {
-  console.log("Updated State ", store.getState());
-});
+// store.subscribe(() => {
+//   console.log("Updated State ", store.getState());
+// });
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
