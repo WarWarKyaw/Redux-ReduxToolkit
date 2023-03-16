d2443289096f7549d2695a3e7a1e9662063aacbd
War War Kyaw <warwar67890@gmail.com>
2023-03-16 21:59:51 +0630

    Configuration of  Store and adding iceCreamSlice

---
+++ rtk-demo/index.js
@@ -0,0 +1,20 @@
+const store = require("./app/store");
+const cakeActions = require("./app/features/cake/cakeSlice").cakeActions;
+const iceCreamActions =
+  require("./app/features/icecream/icecreamSlice").iceCreamActions;
+
+console.log("Initial State ", store.getState());
+
+store.subscribe(() => {
+  console.log("Updated State ", store.getState());
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(3));
